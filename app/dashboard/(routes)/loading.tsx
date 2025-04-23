ebb1f2f24e41472a71cbf99412ee66ae29a48a7c
Wilberk <ledezma.wilberk@gmail.com>
2025-04-23 13:56:25 -0400

    feat: add loading component for dashboard routes

--- app/dashboard/(routes)/loading.tsx
+++ app/dashboard/(routes)/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center h-full w-full">
+    <div className="flex justify-center items-center h-screen w-full">
       <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
     </div>
   );
